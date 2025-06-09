b308927eda80141f73e62bb5c60c75007564efd8
Paul <pauleatonc@gmail.com>
2025-06-09 15:21:47 -0400

    WIP: First Render Configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_CLIENT_ID: string
   readonly VITE_API_URL: string
+  readonly VITE_API_VERSION: string
   // Agrega aquí otras variables de entorno que uses
 }
 
